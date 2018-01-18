98a8458b0c9b8f15fe7a5279e508624219fc9a1b
F-Tang Steve <ftangsteve@gmail.com>
2018-01-17 23:06:08 -0500

    Create numberchain program

---
+++ Number.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberChain
+{
+    class Number
+    {
+        public ulong value;
+        public string wordsAnd;
+        public int valueAnd;
+        public string wordsPlain;
+        public int valuePlain;
+
+        public string[] illions;
+        public string[] tensTypes;
+        public string[] onesTeens;
+
+        public Number(ulong value, string[] illions, string[] tensTypes, string[] onesTeens)
+        {
+            this.value = value;
+            this.illions = illions;
+            this.tensTypes = tensTypes;
+            this.onesTeens = onesTeens;
+            wordsAnd = FindWords(this.value, true);
+            wordsPlain = FindWords(this.value, false);
+        }
+
+        public Number(ulong value, Number givenNum, string[] illions, string[] tensTypes, string[] onesTeens)
+        {
+            this.value = value;
+            this.illions = illions;
+            this.tensTypes = tensTypes;
+            this.onesTeens = onesTeens;
+            this.wordsAnd = FindWords(this.value, true, givenNum);
+            this.wordsPlain = FindWords(this.value, false, givenNum);
+        }
+
+        public void LoadValueTables()
+        {
+            illions = LoadCSV("..\\..\\Illions.csv");
+            tensTypes = LoadCSV("..\\..\\TensTypes.csv");
+            onesTeens = LoadCSV("..\\..\\OnesTeens.csv");
+        }
+        /// <summary>
+        /// This loads a comma seperated value file into the program.
+        /// </summary>
+        /// <param name="filePath">The path of the file to be loaded.</param>
+        /// <returns>Returns in a string array.</returns>
+        public string[] LoadCSV(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            var lines = new List<string>();
+            int Row = 0;
+            while (!sr.EndOfStream)
+            {
+                string Line = sr.ReadLine();
+                lines.Add(Line);
+                Row++;
+            }
+
+            return (lines.ToArray());
+
+        }
+
+        /// <summary>
+        /// This splits up and builds the word for a number from scratch. Hopefully I'll have a (probably dynamic programming)
+        /// algorithm to do it with the other constructor but hey, this is good backup.
+        /// </summary>
+        /// <param name="tempVal">The value to calculate from.</param>
+        /// <param name="withAnd">Whether or not this iteration uses the word and near the end.</param>
+        /// <returns>This returns the string of the final word of the number. Gonna split this off from the one that counts the letters.</returns>
+        public string FindWords(ulong tempVal, bool withAnd)
+        {
+            string concatString = "";
+
+            // The largest of the 'illions' in value.
+            ushort illionsMag = (ushort)(Math.Floor(Math.Log10(tempVal)) / 3);
+            // The number of digits in value.
+            ushort digitNum = (ushort)Math.Floor(Math.Log10(tempVal) + 1);
+            // The number of relevent digits in the largest 'illions' section. So if it's twelve million, this will return two because twelve has
+            // two digits. If it was one hundred and seven billion ninety two million and five (in numbers, not words), this will return 3 as there are three sig figs in one hundred and seven.
+            ushort illionsPre = (ushort)(((digitNum) % 3));
+            if (illionsPre == 0)
+                illionsPre = 3;
+
+            double illionsVal;
+            // powVal here is the first illions it needs to find. We know what to use as all the illions are at a multiple of three, plus one.
+            ushort powVal = (ushort)(digitNum - illionsPre);
+            // This is an important loop. This splits up the number by 'illion' and finds
+            // the value for each section through recursion.
+            while (tempVal > 1000)
+            {
+                illionsVal = Math.Floor(((double)tempVal / Math.Pow(10, powVal)));
+                tempVal -= Convert.ToUInt64(illionsVal * Math.Pow(10, powVal));
+                powVal -= 3;
+
+                concatString += IllionsString(illionsMag, (ushort)illionsVal, withAnd) + " ";
+
+                illionsMag -= 1;
+            }
+
+            // 123, 456, 789
+            //  23, 456, 789
+            //   3, 456, 789
+
+            concatString += DredsTensOnesVal((ushort)tempVal, withAnd, true);
+
+            return (concatString);
+        }
+
+        /// <summary>
+        /// This is cool, it takes a given Number and pulls it to bits, allowing it to save computation time.
+        /// </summary>
+        /// <param name="tempVal">The value to calculate from.</param>
+        /// <param name="withAnd">Whether or not this iteration uses the word and near the end.</param>
+        /// <param name="givenNum">The number to pull apart.</param>
+        /// <returns>Hopefully returns the value more quickly.</returns>
+        public string FindWords(ulong tempVal, bool withAnd, Number givenNum)
+        {
+            string concatString = "";
+
+            // The largest of the 'illions' in value.
+            ushort illionsMag = (ushort)(Math.Floor(Math.Log10(tempVal)) / 3);
+            // The number of digits in value.
+            ushort digitNum = (ushort)Math.Floor(Math.Log10(tempVal) + 1);
+            // The number of relevent digits in the largest 'illions' section. So if it's twelve million, this will return two because twelve has
+            // two digits. If it was one hundred and seven billion ninety two million and five (in numbers, not words), this will return 3 as there are three sig figs in one hundred and seven.
+            ushort illionsPre = (ushort)(((digitNum) % 3));
+            if (illionsPre == 0)
+                illionsPre = 3;
+
+            double illionsVal;
+            // powVal here is the first illions it needs to find. We know what to use as all the illions are at a multiple of three, plus one.
+            ushort powVal = (ushort)(digitNum - illionsPre);
+
+
+            illionsVal = Math.Floor(((double)tempVal / Math.Pow(10, powVal)));
+            tempVal -= Convert.ToUInt64(illionsVal * Math.Pow(10, powVal));
+            powVal -= 3;
+
+            concatString += IllionsString(illionsMag, (ushort)illionsVal, withAnd) + " ";
+
+            illionsMag -= 1;
+
+
+            // 123, 456, 789
+            //  23, 456, 789
+            //   3, 456, 789
+            if (withAnd)
+                concatString += " " + givenNum.wordsAnd;
+            else
+                concatString += " " + givenNum.wordsPlain;
+            return (concatString);
+        }
+
+        /// <summary>
+        /// This finds values lower than one thousand in words.
+        /// </summary>
+        /// <param name="tempVal">The value to be figured out.</param>
+        /// <param name="withAnd">Whether or not we're using and.</param>
+        /// <param name="lastAnd">If this is the last run from a larger section.</param>
+        /// <returns>The string of the words.</returns>
+        public string DredsTensOnesVal(ushort tempVal, bool withAnd, bool lastAnd)
+        {
+            string concatString = "";
+            // This makes dredVal equal to the value of the hundreds column.
+            ushort dredVal = (ushort)Math.Floor((double)tempVal / 100);
+
+            // Now we take the hundreds from tempVal to make it just have our tens and ones.
+            tempVal -= (ushort)(dredVal * 100);
+
+            if (dredVal > 0)
+            {
+                concatString += onesTeens[dredVal] + " " + "Hundred ";
+                if (withAnd && (tempVal > 0))
+                {
+                    concatString += "and ";
+                    lastAnd = false;
+                }
+            }
+
+            if (lastAnd && withAnd && (tempVal > 0) && value > 999)
+                concatString += "and ";
+
+            concatString += TensOnesVal(tempVal);
+
+            return (concatString);
+        }
+
+        /// <summary>
+        /// Finds the tens and/or ones, even if it's a teen value which we
+        /// all know is special in engligh. Just look at fifteen and its
+        /// lack of a 'v' the bugger.
+        /// </summary>
+        /// <param name="tempVal">The value to figure out.</param>
+        /// <returns></returns>
+        public string TensOnesVal(ulong tempVal)
+        {
+            string concatString = "";
+
+            if (tempVal < 20)
+                concatString = onesTeens[tempVal];
+            else
+            {
+                concatString += tensTypes[(int)Math.Floor((double)tempVal / 10) - 2];
+                if ((tempVal % 10) != 0)
+                    concatString += " " + onesTeens[tempVal % 10];
+            }
+
+            return (concatString);
+        }
+
+        /// <summary>
+        /// This uses the magnitude of the 'illions' (which tells if it's million, trillion, etc.) and the
+        /// value at that magnitude to get the string.
+        /// </summary>
+        /// <param name="illionsMag">The magnitude of the 'illion'.</param>
+        /// <param name="illionsVal">The values at that level (it'll be less than 1000 always).</param>
+        /// <param name="withAnd">Whether it has and.</param>
+        /// <returns>The full string.</returns>
+        public string IllionsString(ushort illionsMag, ushort illionsVal, bool withAnd)
+        {
+            string concatString = "";
+
+            if (illionsVal > 0)
+            {
+                concatString += DredsTensOnesVal(illionsVal, withAnd, false) + " " + illions[illionsMag];
+            }
+
+            return concatString;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Use alt + F2 for the analyser
+
+
+
+namespace NumberChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timerWatch = new Stopwatch();
+            timerWatch.Start();
+
+            string[] illions = LoadCSV("..\\..\\Illions.csv");
+            string[] tensTypes = LoadCSV("..\\..\\TensTypes.csv");
+            string[] onesTeens = LoadCSV("..\\..\\OnesTeens.csv");
+
+            //ulong testNum = 412123456789229;
+            //testNum = 123456789;
+            //long time1 = timerWatch.ElapsedMilliseconds;
+            //ulong testNumFirst = IllionsRounder(testNum);
+            //long time2 = timerWatch.ElapsedMilliseconds;
+            //Console.WriteLine(DateTime.Now);
+            //long time3 = timerWatch.ElapsedMilliseconds;
+            //Number firstNum = new Number(testNumFirst, );
+            //long time4 = timerWatch.ElapsedMilliseconds;
+            //IllionsRounder(testNum);
+            //Number secondNum = new Number(testNum, firstNum);
+            //long time5 = timerWatch.ElapsedMilliseconds;
+
+            //int testNumDigits = (int)Math.Floor(Math.Log10(testNum));
+            //Console.WriteLine(testNumDigits);
+
+            //Number numTwo = new Number(testNum);
+            //Number numThree = new Number(23456789);
+            //Number numFour = new Number(3456789);
+            //int testDiv = testNumDigits / 3;
+
+            //// 42 , 123 , 456 , 789 , 229
+            //// 412 , 123 , 456 , 789 , 229
+            //Console.WriteLine(testNum + ": " + IllionsRounder(testNum));
+            //Console.WriteLine("42123456789229: " + IllionsRounder(42123456789229));
+            //ulong[] testArray = new ulong[5000];
+            int timesRun = 10000000;
+
+            Console.WriteLine("test" + new Number(0, illions, tensTypes, onesTeens).wordsPlain);
+
+            long time6 = timerWatch.ElapsedMilliseconds;
+            //      TestMethodOne(timesRun, illions, tensTypes, onesTeens);
+            long time7 = timerWatch.ElapsedMilliseconds;
+            Console.WriteLine("First set done.");
+            //       System.Threading.Thread.Sleep(2000);
+            long time8 = timerWatch.ElapsedMilliseconds;
+            //  TestMethodTwo(timesRun, illions, tensTypes, onesTeens);
+            long time9 = timerWatch.ElapsedMilliseconds;
+            //Console.WriteLine("Dreds Test: " + numTwo.DredsTensOnesVal(216, true, false));
+            //Console.WriteLine("Numtwo Test: " + numTwo.wordsAnd);
+            //Console.WriteLine("Numthree Test: " + numThree.wordsAnd);
+            //Console.WriteLine("Numfour Test: " + numFour.wordsAnd);
+
+            long time10 = timerWatch.ElapsedMilliseconds;
+            TestMethodThree(2, illions, tensTypes, onesTeens);
+            long time11 = timerWatch.ElapsedMilliseconds;
+
+
+            //Console.WriteLine(testDiv);
+            Console.WriteLine("TimeTest1: " + (time7 - time6));
+            Console.WriteLine("TimeTest2: " + (time9 - time8));
+            Console.WriteLine("TimeTest3: " + (time11 - time10));
+            Console.ReadLine();
+        }
+
+        static ulong IllionsRounder(ulong roundVal)
+        {
+
+            // The number of digits in value.
+            ushort digitNum = (ushort)Math.Floor(Math.Log10(roundVal) + 1);
+            // The number of relevent digits in the largest 'illions' section. So if it's twelve million, this will return two because twelve has
+            // two digits. If it was one hundred and seven billion ninety two million and five (in numbers, not words), this will return 3 as there are three sig figs in one hundred and seven.
+            ushort illionsPre = (ushort)(((digitNum) % 3));
+            if (illionsPre == 0)
+                illionsPre = 3;
+            ushort powVal = (ushort)(digitNum - illionsPre);
+            return (ulong)(roundVal % Math.Pow(10, powVal));
+        }
+
+        static void TestMethodOne(int timesRun, string[] illions, string[] tensTypes, string[] onesTeens)
+        {
+            Number[] testNums = new Number[timesRun];
+            for (int i = 0; i < testNums.Length; i++)
+            {
+                testNums[i] = new Number((ulong)(i + 1000000), illions, tensTypes, onesTeens);
+            }
+        }
+        static void TestMethodTwo(int timesRun, string[] illions, string[] tensTypes, string[] onesTeens)
+        {
+            Number[] testNums = new Number[timesRun];
+            for (int i = 0; i < testNums.Length; i++)
+            {
+                if (i >= 1000)
+                    testNums[i] = new Number((ulong)(i + 1000000), testNums[IllionsRounder((ulong)(i))], illions, tensTypes, onesTeens);
+                else
+                    testNums[i] = new Number((ulong)(i + 1000000), illions, tensTypes, onesTeens);
+            }
+        }
+
+        static void TestMethodThree(int illionsMag, string[] illions, string[] tensTypes, string[] onesTeens)
+        {
+            Number[][] testNums = new Number[illionsMag + 1][];
+            testNums[0] = new Number[1000];
+
+            for (int j = 0; j < testNums[0].Length; j++)
+            {
+                testNums[0][j] = new Number((ulong)(j), illions, tensTypes, onesTeens);
+            }
+
+            for (int i = 1; i <= illionsMag; i++)
+            {
+                ulong count = 0;
+                ulong illionMult = (ulong)Math.Pow(10, i * 3);
+                if (i == 2)
+                    testNums[i] = new Number[illionMult * 90];
+                else
+                testNums[i] = new Number[illionMult * 900];
+                for (int j = 0; j < testNums[i].Length && count < (ulong)testNums[i].Length; j++)
+                {
+                    for (int k = 0; k < i; k++)
+                    {
+                        for (int p = 0; p < testNums[k].Length; p++)
+                        {
+                            if (count == 1000)
+                                Console.WriteLine("Hey");
+                            testNums[i][count] = new Number(testNums[k][p].value + illionMult * ((ulong)j + 1), testNums[k][p], illions, tensTypes, onesTeens);
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        static string[] LoadCSV(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            var lines = new List<string>();
+            int Row = 0;
+            while (!sr.EndOfStream)
+            {
+                string Line = sr.ReadLine();
+                lines.Add(Line);
+                Row++;
+            }
+
+            return (lines.ToArray());
+
+        }
+    }
+}
